764af086a6a4cead0ed5f14720d119da35e49058
fire8327 <fire83274@gmail.com>
2026-01-27 18:59:01 +0300

    feat: slider continue

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -3,5 +3,24 @@ $('#tabs').tabs();
 
 /* swiper */
 const swiper = new Swiper('.swiper', {
-    loop: true,
+    slidesPerView: 1.1,
+    spaceBetween: 15,
+    grabCursor: true,
+
+    breakpoints: {
+      640: {
+        slidesPerView: 1.1,
+        spaceBetween: 15,
+      },
+      1024: {
+        slidesPerView: 1.5,
+        spaceBetween: 20,    
+        centeredSlides: true,
+      },
+      1280: {
+        slidesPerView: 1.7,
+        spaceBetween: 20,
+        centeredSlides: true,
+      }
+    },
 });
